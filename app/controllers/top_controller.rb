a721589c811f9630341f942735168bf3fab0651b
Ubuntu <ubuntu@ip-172-31-93-25.ec2.internal>
2022-10-26 05:25:10 +0000

    test

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,2 +1,5 @@
 class TopController < ApplicationController
+    def main
+        redirect_to controller: :products, action: :index
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :products
-  resources :car_items, only: [:new,:creaete, :destroy]
+  resources :cart_items, only: [:new,:creaete, :destroy]
   resources :carts, only: [:show]
   root "top#main"
-  get "top/main"
 end
